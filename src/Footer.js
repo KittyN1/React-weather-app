56d810770cda3dc58a4dd43bad0c25448f924f37
KittyN1 <69148342+KittyN1@users.noreply.github.com>
2020-11-09 18:36:24 +0000

    Update locations

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      {" "}
+      <div class="container">
+        <div class="box shadow-sm">
+          <a href="https://github.com/KittyN1/Weather-App">Open-source code</a>{" "}
+          by Sky
+        </div>
+      </div>
+    </div>
+  );
+}
